99ba694b23ac0e07b0da8be7fa32874ef12e5414
adierwanto <adierwanto666@gmail.com>
2020-04-28 14:05:47 +0700

    javascript

---
+++ if-else.js
@@ -0,0 +1,15 @@
+function jalan1() {
+    var nilai = 2;
+    if(nilai === 3){
+        console.log("benar")
+    }else if (nilai === 2) {
+        console.log("dua")
+    }
+    else{
+        console.log("salah")
+    }
+    // if(nilai === 1)console.log("ok")
+    // else console.log("not okay")
+}
+
+jalan1()
