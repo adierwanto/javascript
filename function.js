bbb7947ed696e4f761673a9774afe0b12e34f685
adierwanto <adierwanto666@gmail.com>
2020-04-28 13:06:55 +0700

    commit

---
+++ function.js
@@ -0,0 +1,17 @@
+function jalan1() {
+    console.log("jalan satu")
+}
+
+const jalan2 = function () {
+    console.log("jalan dua")
+}
+
+jalan1()
+jalan2()
+
+
+const jalan3 = ()=>{
+    console.log("jalan tiga")
+}
+
+jalan3()
