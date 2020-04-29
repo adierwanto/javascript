275df15a01268c983842bcdb21b35a299dfeda1f
adierwanto <adierwanto666@gmail.com>
2020-04-30 00:08:35 +0700

    javascript

---
+++ for.js
@@ -0,0 +1,6 @@
+function perulangan() {
+    for (var index = 0; index < 10; index++) {
+console.log("jalan :", index)        
+    }
+}
+perulangan()
---
+++ nested-if.js
@@ -0,0 +1,17 @@
+var x = 25;
+var y = 30;
+var z = 10;
+
+if(x>y){
+    if (x>z){
+        console.log("x adalah yg paling besar");
+    }else{
+        console.log("nilai x adalah yg terbesar kedua");
+    }
+}else{
+    if(x<z){
+        console.log("x adalah yg terkecil");
+    }else{
+        console.log("nilai x adalah yg terkecil kedua");
+    }
+}
